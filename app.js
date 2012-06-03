4627e237e83ba99c37f252cb01d55075e96d71a0
Augustin Bralley <augustin@datacosmos.com>
2012-06-02 21:13:22 -0700

    latest working version

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ var querystring = require('querystring');
 var request = require('request');
 var sprintf = require('sprintf').sprintf;
 var OAuth2 = require('oauth').OAuth2;
-var Sequelize = require('sequelize');
+var sessionSecret = '42';
+
+//var Sequelize = require('sequelize');
 
 // Create an HTTP server
 var app = express.createServer();
-var dbconn = {};
-require('./config/environment.js')(app, dbconn, express);
-console.log(sprintf("Database connecting: Host: %s:%d, DB: %s, User: %s", dbconn.host, dbconn.port, dbconn.name, dbconn.user));
-var sequelize = new Sequelize(dbconn.name, dbconn.user, dbconn.password, {
-                             host: dbconn.host,
-                             port: dbconn.port,
-                             dialect: 'postgres'});
+app.configure(function() {
+     app.use(express.logger());
+     app.use(express.bodyParser());
+     app.use(express.cookieParser());
+     app.use(express.session({
+        secret: sessionSecret
+     }));
+     app.use(app.router);
+      app.use(express.static(__dirname + '/public'));
+      app.port = 8043;
+     app.hostBaseUrl = process.argv[4] || 'http://localhost:' + app.port;
+     
+  });
 
 var apiBaseUrl = process.argv[5] || 'https://api.singly.com';
 
@@ -36,6 +44,7 @@ var oa = new OAuth2(clientId, clientSecret, apiBaseUrl);
 
 // A convenience method that takes care of adding the access token to requests
 function getProtectedResource(path, session, callback) {
+   console.log(path);
    oa.getProtectedResource(apiBaseUrl + path, session.access_token, callback);
 }
 
@@ -63,37 +72,95 @@ function getLink(prettyName, profiles, token) {
       prettyName);
 }
 
-// Node models for the backend
-var User = sequelize.define('User', {
-  name: Sequelize.STRING,
-},{
-  paranoid: true
-});
-
-var Note = sequelize.define('Note', {
-  content: Sequelize.TEXT,
-},{
-  paranoid: true
-});
-
-sequelize.sync().success(function() {
-  console.log("Successfully sync'd models and tables.");
-}).error(function(error) {
-  console.log("Unable to sync models and tables:");
-  console.log(error);
-});
 
 // Use ejs instead of jade because HTML is easy
 app.set('view engine', 'ejs');
 
-app.get('/users', function (req, res) {
+app.get('/user', function (req, res) {
   var users = {};
   res.writeHead(200, {"Content-Type": "text/html"});
   res.write(JSON.stringify(users));
   res.end();
 });
 
+
+app.get('/friend', function(req, res) {
+
+   getProtectedResource('/types/statuses_feed', req.session, function(err, statuses){
+      //console.log(statuses); 
+      
+      var rStatus = JSON.parse(statuses)[Math.floor(Math.random()*20)];
+      console.log(rStatus.idr);  
+
+      /***** TODO *******/
+      // GET DETAILS OF FRIEND FROM ALL SERVICES
+
+      if(rStatus.idr.indexOf("twitter") != -1)
+      //is a twitter status
+      {
+         //Get twitter user details
+         getProtectedResource('/by/contact/twitter/' + rStatus.data.user.screen_name, req.session, function(err, item) {
+            
+            var contact = JSON.parse(item)[0]; 
+            console.log(contact);
+            var person = { 
+               "id" : contact.idr,
+               "name" : contact.data.name,
+               "username" : contact.data.screen_name,
+               "description" : contact.data.description,
+               "location" : contact.data.location,
+               "status" : contact.data.status.text,
+               "photo" : contact.data.profile_image_url }
+            
+            console.log(person);
+
+            res.write(JSON.stringify(person));
+            res.end();
+
+         });
+
+         //Search contacts for other accounts -- TODO
+         //singly.get('/types/contacts?q=' + rStatus.data.name, null, function(item){
+         //   console.log(item);
+         //});
+
+      }
+      else if(rStatus.idr.indexOf("facebook") != -1)
+      //is a facebook status
+      {
+         //Get facebook user details
+         getProtectedResource('/by/contact/facebook/' + rStatus.data.from.id, req.session, function(err, item) {
+            console.log(item); 
+            var contact = JSON.parse(item)[0]; 
+            
+            var person = { 
+               "id" : contact.idr,
+               "name" : contact.data.name,
+               "username" : contact.data.username,
+               "description" : (contact.data.bio) ? contact.data.bio : contact.data.quotes,
+               "location" : contact.data.location.name,
+               "status" : (rStatus.data.message) ? rStatus.data.message : rStatus.data.story,
+               "photo" : contact.oembed.thumbnail_url,
+               "profession" : contact.data.work && contact.data.work[0].employer.name }
+            
+            console.log(person);
+
+            res.write(JSON.stringify(person));
+            res.end();
+
+         });               
+        
+      }
+   });
+   
+});
+
+
 app.get('/', function(req, res) {
+   res.render('splash', {session: req.session});
+});
+   
+app.get('/auth', function(req, res){
    var i;
    var services = [];
 
@@ -106,10 +173,14 @@ app.get('/', function(req, res) {
    }
 
    // Render out views/index.ejs, passing in the array of links and the session
-   res.render('index', {
+   res.render('auth', {
       services: services,
       session: req.session
    });
+});
+
+app.get('/pipe', function(req, res) {
+   res.render('app', {session: req.session});
 });
 
 app.get('/callback', function(req, res) {
@@ -143,7 +214,7 @@ app.get('/callback', function(req, res) {
 
          req.session.profiles = profilesBody;
 
-         res.redirect('/');
+         res.redirect('/pipe');
       });
    });
 });
